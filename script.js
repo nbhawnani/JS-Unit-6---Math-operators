bcdaa34112c486768f52e86d0c5c06de49cf4403
Nisha Brijesh <92055350+nbhawnani@users.noreply.github.com>
2022-04-08 23:17:46 +0000

    4/8

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function GenerateResult(){
-  
-console.log(document.getElementById("op1").value +  document.getElementById("op2").value);  document.getElementById("result").value=eval(document.getElementById("op1").value +  document.getElementById("operator").value+document.getElementById("op2").value)
+
+console.log(document.getElementById("op1").value +  document.getElementById("operator").value+document.getElementById("op2").value); 
+
+if (document.getElementById("op2").value.length>0){
+      document.getElementById("result").value=eval(document.getElementById("op1").value +      document.getElementById("operator").value+document.getElementById("op2").value);
+} 
+
 }
+
+  
