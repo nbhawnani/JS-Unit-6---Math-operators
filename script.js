a91f001425d713a45c1c79626397021924d20e75
Nisha Brijesh <92055350+nbhawnani@users.noreply.github.com>
2022-04-08 22:12:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function GenerateResult(){
+  
+console.log(document.getElementById("op1").value +  document.getElementById("op2").value);  document.getElementById("result").value=eval(document.getElementById("op1").value +  document.getElementById("operator").value+document.getElementById("op2").value)
+}
